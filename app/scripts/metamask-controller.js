c1e5acdebf0c16f7dc5ec1b17a815d6ded1ca0bd
Niranjana Binoy <43930900+NiranjanaBinoy@users.noreply.github.com>
2021-06-27 11:54:17 -0400

    TokenListcontroller integration

--- app/scripts/metamask-controller.js
+++ app/scripts/metamask-controller.js
@@ -25,7 +25,7 @@ import {
   PhishingController,
   NotificationController,
   GasFeeController,
-  TokenListController,
+  TokenListController
 } from '@metamask/controllers';
 import { TRANSACTION_STATUSES } from '../../shared/constants/transaction';
 import { MAINNET_CHAIN_ID } from '../../shared/constants/network';
@@ -226,6 +226,15 @@ export default class MetamaskController extends EventEmitter {
       messenger: currencyRateMessenger,
       state: initState.CurrencyController,
     });
+    const tokenListMessenger = controllerMessenger.getRestricted({
+      name: 'TokenListController',
+    });
+    this.tokenListController = new TokenListController({
+      chainId: this.networkController.getCurrentChainId(),
+      onNetworkStateChange: this.networkController.store.subscribe.bind(this.networkController.store),
+      messenger: tokenListMessenger,
+      state: initState.tokenListController,
+    });
 
     const tokenListMessenger = controllerMessenger.getRestricted({
       name: 'TokenListController',
@@ -366,6 +375,7 @@ export default class MetamaskController extends EventEmitter {
       preferences: this.preferencesController,
       network: this.networkController,
       keyringMemStore: this.keyringController.memStore,
+      tokenList: this.tokenListController
     });
 
     this.addressBookController = new AddressBookController(
